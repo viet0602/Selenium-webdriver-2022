333a08be26e10fdda31407c51ebc74409d6c2acb
viet0602 <viet0602@gmail.com>
2022-05-04 23:20:21 +0700

    add one more test case

--- SeleniumAPI/WebElement.java
+++ SeleniumAPI/WebElement.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -12,7 +13,11 @@ public class WebElement {
 	By emailLocator = By.cssSelector("[id='mail']");
 	By under18Locator = By.cssSelector("[id='under_18']");
 	By educationLocator = By.cssSelector("[id='edu']");
-	
+	By image5HoverText = By.xpath("//img[@alt='User Avatar 05']/parent::div//h5[text()='Name: User5']");
+	By jobRol1 = By.cssSelector("[id='job1']");
+	By jobRol2 = By.cssSelector("[id='job2']");
+	By developmentCheckBox = By.cssSelector("[id='development']");
+	By slide1 = By.cssSelector("[id='slider-1']");
 
 	@BeforeClass
 	public void beforeClass() {
@@ -23,7 +28,7 @@ public class WebElement {
 	}
 
 	@Test
-	public void TC_01_isDisplay() {	
+	public void TC_01_isDisplay() {
 		driver.get("https://automationfc.github.io/basic-form/index.html");
 		Assert.assertTrue(driver.findElement(emailLocator).isDisplayed());
 		driver.findElement(emailLocator).sendKeys("afsdfasfd@gmail.com");
@@ -31,6 +36,25 @@ public class WebElement {
 		driver.findElement(under18Locator).click();
 		Assert.assertTrue(driver.findElement(educationLocator).isDisplayed());
 		driver.findElement(educationLocator).sendKeys("Hanu university");
+		Assert.assertFalse(driver.findElement(image5HoverText).isDisplayed());
 	}
 
+	@Test
+	public void TC_02_isEnable() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		Assert.assertTrue(driver.findElement(emailLocator).isEnabled());
+		Assert.assertTrue(driver.findElement(under18Locator).isEnabled());
+		Assert.assertTrue(driver.findElement(educationLocator).isEnabled());
+		Assert.assertTrue(driver.findElement(jobRol1).isEnabled());
+		Assert.assertTrue(driver.findElement(jobRol2).isEnabled());
+		Assert.assertTrue(driver.findElement(developmentCheckBox).isEnabled());
+		Assert.assertTrue(driver.findElement(slide1).isEnabled());
+	}
+	
+	
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
 }
